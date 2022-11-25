1e9df393b3ec013e23a4a50eda3f61ee4e93b45e
Kennedy Moranga <kennedy.moranga@student.moringaschool.com>
2022-11-25 10:34:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute ('id','victory');
+newHeader.textContent = "Kennedy is the champion";
+document.head.append(newHeader);
